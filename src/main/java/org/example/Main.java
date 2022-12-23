c28e317978347992c00a466d44d6003de0fb5be0
bahamedabdullah <ahamedabdullahb@outlook.com>
2022-12-23 15:04:23 +0530

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().timeouts().implicitlyWait(10000,TimeUnit.MILLISECONDS);
+        driver.get("https://www.amazon.in");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Headphones");
+        Thread.sleep(3000);
+        System.out.println(driver.findElement(By.id("nav-flyout-searchAjax")).getText());
+        driver.findElement(By.id("nav-search-submit-button")).click();
+
+
+        Thread.sleep(10000);
+        //driver.close();
+        driver.quit();
+
     }
 }
---
+++ src/main/java/org/example/PageObject.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PageObject {
+    WebDriver driver;
+    @FindBy(id="twotabsearchtextbox")
+    WebElement searchBox;
+
+    @FindBy(id="nav-flyout-searchAjax")
+    WebElement searchTip;
+
+    @FindBy(id="nav-search-submit-button")
+    WebElement searchButton;
+
+    public PageObject(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+
+
+
+}
