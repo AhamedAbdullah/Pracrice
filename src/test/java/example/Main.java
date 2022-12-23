ac3ea5a6470594c1e94e37cd722c66a8498e2032
bahamedabdullah <ahamedabdullahb@outlook.com>
2022-12-23 21:44:16 +0530

    First commit

--- src/test/java/example/Main.java
+++ src/test/java/example/Main.java
@@ -1,14 +1,22 @@
-package org.example;
+package example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    @Test
+    public void amazon() throws InterruptedException, IOException {
         System.out.println("Hello world!");
+        Properties prop = new Properties();
+        prop.load(this.getClass().getResourceAsStream("/my.properties"));
+        System.out.println(prop.getProperty("version"));
+        System.out.println(prop.getProperty("user"));
         WebDriver driver = WebDriverManager.chromedriver().create();
         driver.manage().timeouts().implicitlyWait(10000,TimeUnit.MILLISECONDS);
         driver.get("https://www.amazon.in");
--- src/test/java/example/PageObject.java
+++ src/test/java/example/PageObject.java
@@ -1,4 +1,4 @@
-package org.example;
+package example;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
